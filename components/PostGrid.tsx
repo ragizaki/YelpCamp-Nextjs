a2a2b84975917408c5a82e9dd57653299c0c3308
Zaki Machfj <zakimachfj@gmail.com>
2023-03-03 22:42:50 -0500

    add responsive styles to xl breakpoint

--- components/PostGrid.tsx
+++ components/PostGrid.tsx
@@ -8,7 +8,11 @@ interface PostGridProps {
 const PostGrid: React.FC<PostGridProps> = ({ posts }) => {
   return (
     <Grid
-      templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
+      templateColumns={{
+        base: "repeat(1, 1fr)",
+        md: "repeat(3, 1fr)",
+        "2xl": "repeat(4, 1fr)",
+      }}
       gap={6}
     >
       {posts.map((post) => (
